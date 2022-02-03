b4080e6f85a59a0bd4c45be7c29f25dea852d4be
Alexander Blackman <ablackman@MacBook-Air-Alexander.local>
2022-02-03 14:13:04 +0300

    migrating from my School21 repository

---
+++ get_next_line.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wbernard <wbernard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 17:15:49 by wbernard          #+#    #+#             */
+/*   Updated: 2022/01/10 13:09:30 by wbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_get_string(char **rest)
+{
+	char	*end_of_line;
+	char	*result;
+	char	*temp;
+
+	if (!*rest)
+		return (NULL);
+	if (!**rest)
+	{
+		free(*rest);
+		return (NULL);
+	}
+	end_of_line = ft_strchr(*rest, '\n');
+	if (!end_of_line)
+	{
+		result = ft_substr(*rest, 0, ft_strlen(*rest));
+		free(*rest);
+		*rest = NULL;
+		return (result);
+	}
+	result = ft_substr(*rest, 0, (end_of_line - *rest + 1));
+	temp = *rest;
+	*rest = ft_substr(end_of_line + 1, 0, ft_strlen(end_of_line + 1));
+	free(temp);
+	return (result);
+}
+
+static	char	*ft_read(int fd, char **rest, char *buffer)
+{
+	int		bytes_count;
+	char	*temp;
+
+	if (!*rest || !(ft_strchr(*rest, '\n')))
+	{
+		bytes_count = read(fd, buffer, BUFFER_SIZE);
+		while (bytes_count)
+		{
+			buffer[bytes_count] = 0;
+			if (!*rest)
+				*rest = ft_substr(buffer, 0, bytes_count);
+			else
+			{
+				temp = *rest;
+				*rest = ft_strjoin(*rest, buffer);
+				free(temp);
+			}
+			if (ft_strchr(buffer, '\n'))
+				break ;
+			bytes_count = read(fd, buffer, BUFFER_SIZE);
+		}
+	}
+	return (*rest);
+}
+
+char	*get_next_line(int fd)
+{
+	char		buffer[BUFFER_SIZE + 1];
+	static char	*rest;
+	char		*result;
+
+	if (BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	ft_read(fd, &rest, buffer);
+	result = ft_get_string(&rest);
+	return (result);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wbernard <wbernard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 17:15:55 by wbernard          #+#    #+#             */
+/*   Updated: 2022/01/10 13:20:29 by wbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	const char	*str;
+
+	str = s;
+	while (*str++)
+		;
+	return (str - s - 1);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	const char		*src;
+	unsigned char	value;
+
+	src = s;
+	value = (unsigned char) c;
+	while (*src)
+	{
+		if (*src == value)
+			return ((char *) src);
+		src++;
+	}
+	if (value == '\0')
+		return ((char *) src);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	char	*work;
+	int		result_size;
+
+	if (!s1 || !s2)
+		return (NULL);
+	result_size = ft_strlen(s1) + ft_strlen(s2);
+	result = (char *) malloc (result_size + 1);
+	if (!result)
+		return (NULL);
+	work = result;
+	if (s1)
+		while (*s1)
+			*work++ = *s1++;
+	if (s2)
+		while (*s2)
+			*work++ = *s2++;
+	*work = 0;
+	return (result);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char		*str;
+	char		*result;
+	size_t		i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		str = (char *) malloc (sizeof(char));
+		*str = 0;
+		return (str);
+	}
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	result = malloc ((len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	while (i < len && s[start])
+		result[i++] = s[start++];
+	result[i] = 0;
+	return (result);
+}
